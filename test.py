681c195c83879c2bd665c57e2b634604aae6a6e2
mario.xu <zhengxu32@gmail.com>
2017-01-03 19:48:34 -0500

    answered some questions in outliers, change pickle import so that it works under windows.

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn import svm
+
+X, y = np.arange(10).reshape((5, 2)), range(5)
+
+# print(X)
+# print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+# print(X_train)
+# print(X_test)
+
+iris = datasets.load_iris()
+print(iris)
