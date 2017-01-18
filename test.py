1fd4e99f6c6e44689d6163cf6a796a21216a8533
mario.xu <zhengxu32@gmail.com>
2017-01-17 20:38:57 -0500

    answered some questions in text learning

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
+from nltk.corpus import stopwords
 
-X, y = np.arange(10).reshape((5, 2)), range(5)
+print(len(stopwords.words('english')))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-import heapq
-
-print(heapq.nlargest(2, [(1,2),(3,4),(5,2),(6,7)], key=lambda i: i[1]))
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -3,6 +3,7 @@
 from nltk.stem.snowball import SnowballStemmer
 import string
 
+
 def parseOutText(f):
     """ given an opened email file f, parse out all text below the
         metadata block at the top
@@ -15,39 +16,37 @@ def parseOutText(f):
         text = parseOutText(f)
         
         """
-
-
-    f.seek(0)  ### go back to beginning of file (annoying)
+    f.seek(0)  # go back to beginning of file (annoying)
     all_text = f.read()
 
-    ### split off metadata
+    # split off metadata
     content = all_text.split("X-FileName:")
     words = ""
     if len(content) > 1:
-        ### remove punctuation
-        text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
+        # remove punctuation
+        translator = str.maketrans('', '', string.punctuation)
+        text_string = content[1].translate(translator)
 
-        ### project part 2: comment out the line below
-        words = text_string
+        # project part 2: comment out the line below
+        # words = text_string
 
-        ### split the text string into individual words, stem each word,
-        ### and append the stemmed word to words (make sure there's a single
-        ### space between each stemmed word)
-        
-
-
-
+        # split the text string into individual words, stem each word,
+        individualWords = text_string.split()
+        stemmedWordsList = []
+        stemmer = SnowballStemmer('english')
+        for word in individualWords:
+            stemmedWordsList.append(stemmer.stem(word))
+        # and append the stemmed word to words (make sure there's a single
+        words = ' '.join(stemmedWordsList)
+        # space between each stemmed word)
 
     return words
 
-    
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
     text = parseOutText(ff)
-    print text
-
-
+    print(text)
 
 if __name__ == '__main__':
     main()
