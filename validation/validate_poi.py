a08b5701387aaf93e27271083883ea112178c64d
mario.xu <zhengxu32@gmail.com>
2017-01-25 13:03:57 -0500

    ...

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -15,18 +15,14 @@ import sys
 sys.path.append("../tools/")
 from tools.feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
+# first element is our labels, any added elements are predictor
+# features. Keep this the same for the mini-project, but you'll
+# have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-
-
-### it's all yours from here forward!  
-
-
+# it's all yours from here forward!
